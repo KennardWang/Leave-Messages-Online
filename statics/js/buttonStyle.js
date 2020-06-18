9c734b2052c87466aeadad9b09d376f1f7c62c8a
YY <57723061+KennardWang@users.noreply.github.com>
2020-06-18 11:56:29 +0800

    Add files via upload

---
+++ statics/js/buttonStyle.js
@@ -0,0 +1,11 @@
+
+function mouseOver(e) {
+    e.style.color = "azure";
+    e.style.backgroundColor = "rgba(20,20,20,0.7)";
+}
+
+function mouseOut(e) {
+    e.style.color = "black";
+    e.style.backgroundColor = "rgba(200,200,200,0.35)";
+}
+
